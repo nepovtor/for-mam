f60cfd8db72cfcf8e586882aa25988885d9ddab8
nepovtor <111302019+nepovtor@users.noreply.github.com>
2025-06-05 14:23:53 +0300

    update tailwind
    Good job's

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,         // ⬅️ позволяет принимать подключения извне
+    port: 5163,         // можешь поменять при необходимости
+    strictPort: true
+  }
 })
